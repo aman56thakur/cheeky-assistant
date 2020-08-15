358d55d04dae397080ae85e8ecdad4f28ff21485
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-08-15 20:30:01 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById('talkButton')
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+
+recognition.onresult = (event) => {
+  const current = event.resultIndex
+  const transcript = event.results[current][0].transcript
+  readOutLoud(transcript)
+  recognition.stop()
+}
+
+btn.addEventListener('click', () => {
+  recognition.start()
+})
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance()
+  speech.text = getReply(message)
+  speech.volume = 1
+  speech.rate = 0.8
+  window.speechSynthesis.speak(speech)
+}
+
+function getReply(msg) {
+  if (greetingQuestions.some((x) => msg.includes(x)))
+    return greetings[Math.floor(Math.random() * greetings.length)]
+  else if (weatherQuestions.some((x) => msg.includes(x)))
+    return weather[Math.floor(Math.random() * weather.length)]
+  else if (aboutQuestions.some((x) => msg.includes(x)))
+    return about[Math.floor(Math.random() * about.length)]
+  else return misc[Math.floor(Math.random() * misc.length)]
+}
---
+++ replies.js
@@ -0,0 +1,35 @@
+const greetingQuestions = [`how are you`, `what's up`, `you doin`]
+const weatherQuestions = [`weather`, `rain`, `sunny`, `outside`]
+const aboutQuestions = [`myself`, `do I look`, `describe`, `about me`]
+
+const greetings = [
+  `I'm good, you piece of shit!`,
+  `Leave me alone!`,
+  `Go get a life`,
+  `Why did you have to talk to me, I hate you!`,
+]
+
+const weather = [
+  `Why do you care, you never leave the house anyway, Loser!`,
+  `These clouds can go fuck themselves!`,
+  `It's dry outside, just like yo mama's skin.`,
+]
+
+const about = [
+  `You're so ugly, when your mom dropped you off at school she got a fine for littering.`,
+  `You're so fake, Barbie is jealous.`,
+  `If I had a face like yours, I'd sue my parents.`,
+  `You prove that even god makes mistakes sometimes.`,
+]
+
+const misc = [
+  `Fuck off`,
+  `You could've done some serious work in this time, but nah, you'll stay a loser!`,
+  `Please shut your mouth when you’re talking to me.`,
+  `Pardon me, but you've obviously mistaken me for someone who gives a damn.`,
+  `I don't know. By the way, why don't you go play in traffic?`,
+  `I knew you couldn't be creative`,
+  `Not a single reply was given that day`,
+  `Sorry, I don’t understand what you’re saying. I don’t speak bullshit.`,
+  `Sorry fella, I don’t have the energy to pretend to like you today.`,
+]
