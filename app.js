939564dbab9bde94866851cbfa5851972187baf6
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-08-15 20:47:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function readOutLoud(message) {
   const speech = new SpeechSynthesisUtterance()
   speech.text = getReply(message)
   speech.volume = 1
-  speech.rate = 0.8
+  speech.rate = 1
   window.speechSynthesis.speak(speech)
 }
 
